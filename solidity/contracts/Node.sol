e0a368c3d885d4a646435eaf051f684999f084b8
ancjf <ancjf@163.com>
2018-04-17 17:18:45 +0800

    node 合约更新

--- solidity/contracts/Node.sol
+++ solidity/contracts/Node.sol
@@ -2,54 +2,74 @@ pragma solidity ^0.4.4;
 
 import "./Strings.sol";
 
-contract Node{
+contract Ownable {
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+    * @dev Throws if called by any account other than the owner.
+    */
+    modifier onlyOwner() {
+        require(owner == address(0x0000000000000000000000000000000000000000) || msg.sender == owner);
+        _;
+    }
+
+    /**
+    * @dev Allows the current owner to transfer control of the contract to a newOwner.
+    * @param newOwner The address to transfer ownership to.
+    */
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
+
+contract Node is Ownable{
     using Strings for *;
 
-    enum NodeType{
-        None,
-        Core,   // 核心
-        Full,   // 全节点
-        Light   //  轻节点
-    }
-    
     struct NodeInfo{
-        string    ip;            
-        uint    port;
-        uint8    category;
+        string    property;
+        bool    created;
     }
 
     mapping(string =>NodeInfo) m_nodedata;
     string[] m_nodeids;
 
-	function Node(){
-		registerNode("%", "*", 0, 1);
-	}
-	
     //登记节点信息
     event registerNodeEvent(bool ret);
-    function registerNode (string _id, string _ip, uint _port, uint8 _category)  returns(bool) {
-        bool find=false;
-
-        for(uint i=0; i < m_nodeids.length; i++){
-            if(_id.toSlice().compare(m_nodeids[i].toSlice()) == 0)
-            {
-                find=true;
-                break;
-            }
+    function registerNode(string _id, string _property) onlyOwner{
+        if(!m_nodedata[_id].created){
+            m_nodeids.push(_id);
         }
 
-        if( find ){
-            registerNodeEvent(true);
-            return false;
-        }
-
-        m_nodeids.push(_id);
-        m_nodedata[_id] = NodeInfo(_ip, _port, _category);
-
+        m_nodedata[_id] = NodeInfo(_property, true);
         registerNodeEvent(true);
-        return true;
     }
-  
+
+    //登记节点信息
+    event unregisterNodeEvent(bool ret);
+    function unregisterNode(string _id) onlyOwner{
+        if(m_nodedata[_id].created){
+            uint i = 0;
+            for(; i < m_nodeids.length; i++){
+                if(_id.toSlice().compare(m_nodeids[i].toSlice()) == 0)
+                    break;
+            }
+
+            for(; i < m_nodeids.length-1; i++){
+                m_nodeids[i] = m_nodeids[i+1];
+            }
+
+            m_nodeids.length--;
+            m_nodedata[_id].created = false;
+            unregisterNodeEvent(true);
+        }else{
+            unregisterNodeEvent(false);
+        }
+    }
+
     //查询节点信息
     function getNode(string _id) public constant returns(string){
         var slice = "{".toSlice();
@@ -57,34 +77,23 @@ contract Node{
         slice = slice.concat("\"id\":\"".toSlice()).toSlice();
         slice = slice.concat(_id.toSlice()).toSlice();
 
-        slice = slice.concat("\",\"ip\":\"".toSlice()).toSlice();
-        slice = slice.concat(m_nodedata[_id].ip.toSlice()).toSlice();
-
-        slice = slice.concat("\",\"port\":\"".toSlice()).toSlice();
-        uint port = uint(m_nodedata[_id].port);
-        slice = slice.concat(port.uintToBytes().bytes32ToString().toSlice()).toSlice();
-
-        slice = slice.concat("\",\"type\":\"".toSlice()).toSlice();
-        uint category = uint(m_nodedata[_id].category);
-        slice = slice.concat(category.uintToBytes().bytes32ToString().toSlice()).toSlice();
+        slice = slice.concat("\",\"property\":\"".toSlice()).toSlice();
+        slice = slice.concat(m_nodedata[_id].property.toSlice()).toSlice();
 
         slice = slice.concat("\"}".toSlice()).toSlice();
         var ret = slice.toString();
         return ret;
     }
 
-    function getNodeType(uint8 _category) public constant returns(string){
+    function getAllNode() public constant returns(string){
         var slice = "[".toSlice();
         for(uint i = 0; i < m_nodeids.length; i++){
-            if(_category == uint(NodeType.None) || _category == m_nodedata[m_nodeids[i]].category){
-                var str = getNode(m_nodeids[i]);
-                slice = slice.concat(str.toSlice()).toSlice();
+            var str = getNode(m_nodeids[i]);
+            slice = slice.concat(str.toSlice()).toSlice();
 
-                if(i+1 < m_nodeids.length){
-                    slice = slice.concat(",".toSlice()).toSlice();
-                }
+            if(i+1 < m_nodeids.length){
+                slice = slice.concat(",".toSlice()).toSlice();
             }
-
         }
 
         slice = slice.concat("]".toSlice()).toSlice();
